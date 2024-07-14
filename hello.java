4adec728797122afd46d6e3af2bd3276a9c2f5ff
yash sharma <yash.sharma23@spit.ac.ine.com>
2024-07-14 11:54:43 +0530

    hello 1.0

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello {
     public static void main(String [] args)
     {
-        System.out.println("hello world by java");
+        System.out.println("hello world by java 1");
+        System.out.println("hello world by java 2");
     }
 }
